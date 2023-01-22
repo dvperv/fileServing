60a127c8a7411d7af9c3fbb8b31ee37d2e97e091
Dmitry Pervoukhin <dvperv@gmail.com>
2023-01-22 22:18:36 +0300

    Фронт: итератор по массиву, проброс параметров вниз

--- src/App.svelte
+++ src/App.svelte
@@ -1,6 +1,5 @@
 <script>
   import svelteLogo from './assets/svelte.svg'
-  import Counter from './lib/Counter.svelte'
   import FileList from "./lib/FileList.svelte";
 </script>
 
@@ -21,15 +20,6 @@
   <h1>Files</h1>
 
   <div class="card">
-    <Counter />
     <FileList/>
   </div>
-
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank" rel="noreferrer">SvelteKit</a>, the official Svelte app framework powered by Vite!
-  </p>
-
-  <p class="read-the-docs">
-    Click on the Vite and Svelte logos to learn more
-  </p>
 </main>
